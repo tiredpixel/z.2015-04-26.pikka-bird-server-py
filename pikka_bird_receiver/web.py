345313061b4b63988517d1ad201a8cc05af4c031
tiredpixel <tp@tiredpixel.com>
2015-04-03 22:05:37 +0100

    Wrap Flask app creation in create_app() , adding DEBUG .

--- pikka_bird_receiver/web.py
+++ pikka_bird_receiver/web.py
@@ -1,14 +1,22 @@
 #!/usr/bin/env python
 
+import os
 from flask import Flask, jsonify
 
 import pikka_bird_receiver.routes.statics
 
 
-app = Flask(__name__)
-
-app.register_blueprint(pikka_bird_receiver.routes.statics.statics)
+def create_app(debug=False):
+    app = Flask(__name__)
+    app.debug = debug
+    
+    app.register_blueprint(pikka_bird_receiver.routes.statics.statics)
+    
+    return app
 
 
 if __name__ == "__main__":
+    debug = (int(os.environ['DEBUG']) == 1)
+    
+    app = create_app(debug=debug)
     app.run()
