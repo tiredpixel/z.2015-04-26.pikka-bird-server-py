23121ed4336604be45c82e0d7ad762a935a32a95
tiredpixel <tp@tiredpixel.com>
2015-04-03 17:27:21 +0100

    Set up structure, __version__, package metadata, pytest, script.

---
+++ setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+import pikka_bird_receiver
+
+
+setup(
+    name     = 'pikka-bird-receiver',
+    version  = pikka_bird_receiver.__version__,
+    packages = find_packages(),
+    scripts  = ['bin/pikka-bird-receiver'],
+    
+    author       = 'tiredpixel',
+    author_email = 'tp@tiredpixel.com',
+    description  = "Pikka Bird monitoring tool metrics receiver.",
+    license      = '',
+    keywords     = 'monitoring',
+    url          = '',
+    
+    install_requires = [
+    ]
+)
---
+++ test/unit/test_tmp.py
@@ -0,0 +1,4 @@
+class TestTmp:
+    
+    def test_enabled(self):
+        assert True == True
