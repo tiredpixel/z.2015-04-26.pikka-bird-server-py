5d48d8e85a22808e97308083fb32ee6660d9f4e5
tiredpixel <tp@tiredpixel.com>
2015-04-04 18:36:38 +0100

    Auto-migrate database, refactoring into __setup_logger(),__setup_db() .
    - replace `DEBUG` with `LOG_LEVEL`
    
    - refactor `app.py` into `__setup_logger()`, `__setup_db()`, setting
      log-level
    
    - auto-migrate database if `DATABASE_AUTO_MIGRATE` set, keeping to plan
      of minimising steps needed to install or maintain Pikka Bird

--- pikka_bird_receiver/app.py
+++ pikka_bird_receiver/app.py
@@ -1,8 +1,10 @@
+import logging
 import os
+import sys
 from flask import Flask, jsonify
 from flask.ext.sqlalchemy import SQLAlchemy
-from flask.ext.script import Manager
-from flask.ext.migrate import Migrate, MigrateCommand
+import flask.ext.script
+import flask.ext.migrate
 from sqlalchemy import create_engine
 
 import pikka_bird_receiver.routes.statics
@@ -10,23 +12,44 @@ import pikka_bird_receiver.routes.statics
 
 def create_app():
     app = Flask('pikka_bird_receiver')
-    app.debug = (int(os.environ['DEBUG']) == 1)
     
-    app.db = create_engine(os.environ['DATABASE_URI'])
+    app.debug = (os.environ['LOG_LEVEL'] == 'DEBUG')
+    app.db    = create_engine(os.environ['DATABASE_URI'])
     
     app.register_blueprint(pikka_bird_receiver.routes.statics.statics)
+    
+    __setup_logger(app.logger)
+    __setup_db(app)
     
     return app
 
 def create_manager(app):
-    db = SQLAlchemy(app)
-    Migrate(app, db) # attach for Alembic
-    
-    manager = Manager(app)
-    manager.add_command('db', MigrateCommand)
+    manager = flask.ext.script.Manager(app)
+    manager.add_command('db', flask.ext.migrate.MigrateCommand)
     
     return manager
 
+def __setup_logger(logger):
+    formatter = logging.Formatter(
+        '[%(asctime)s] %(levelname)s -- %(name)s: %(message)s')
+    
+    handler = logging.StreamHandler()
+    handler.setFormatter(formatter)
+    
+    logger.setLevel(os.environ['LOG_LEVEL'])
+    logger.addHandler(handler)
+
+def __setup_db(app):
+    auto_migrate = (int(os.environ['DATABASE_AUTO_MIGRATE']) == 1)
+    
+    db = SQLAlchemy(app)
+    flask.ext.migrate.Migrate(app, db) # attach for Alembic
+    
+    if auto_migrate:
+        app.logger.info('DATABASE AUTO-MIGRATING')
+        
+        with app.app_context():
+            flask.ext.migrate.upgrade()
 
 if __name__ == '__main__':
     app     = create_app()
