4d1abd4f4ea0f3ab379080406285b9ee217a3b9a
tiredpixel <tp@tiredpixel.com>
2015-04-04 16:21:10 +0100

    Attach database to app, refactoring.
    - refactor config `web.py` => `app.py`, making `web.py` merely a script
    
    - attach database in `app.py` as `app.db`, reading `DATABASE_URI`
    
    - add `app.py` `create_manager()`, with `db` command
    
    - adjust generated `migrations/env.py` to use `app.db`

--- pikka_bird_receiver/app.py
+++ pikka_bird_receiver/app.py
@@ -1,12 +1,35 @@
+import os
 from flask import Flask, jsonify
+from flask.ext.sqlalchemy import SQLAlchemy
+from flask.ext.script import Manager
+from flask.ext.migrate import Migrate, MigrateCommand
+from sqlalchemy import create_engine
 
 import pikka_bird_receiver.routes.statics
 
 
-def create_app(debug=False):
+def create_app():
     app = Flask('pikka_bird_receiver')
-    app.debug = debug
+    app.debug = (int(os.environ['DEBUG']) == 1)
+    
+    app.db = create_engine(os.environ['DATABASE_URI'])
     
     app.register_blueprint(pikka_bird_receiver.routes.statics.statics)
     
     return app
+
+def create_manager(app):
+    db = SQLAlchemy(app)
+    Migrate(app, db) # attach for Alembic
+    
+    manager = Manager(app)
+    manager.add_command('db', MigrateCommand)
+    
+    return manager
+
+
+if __name__ == '__main__':
+    app     = create_app()
+    manager = create_manager(app)
+    
+    manager.run()
--- pikka_bird_receiver/web.py
+++ pikka_bird_receiver/web.py
@@ -1,14 +1,11 @@
 #!/usr/bin/env python
 
 
-import os
-
 from pikka_bird_receiver.app import create_app
 
 
-debug = (int(os.environ['DEBUG']) == 1)
+app = create_app()
 
-app = create_app(debug=debug)
 
 if __name__ == '__main__':
     app.run()
