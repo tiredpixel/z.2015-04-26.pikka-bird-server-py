1c01f44515146f068fb041328d5016ebbc79652d
tiredpixel <tp@tiredpixel.com>
2015-04-26 15:34:08 +0100

    Package, using setuptools-git.

--- setup.py
+++ setup.py
@@ -8,16 +8,36 @@ import pikka_bird_server
 setup(
     name     = 'pikka-bird-server',
     version  = pikka_bird_server.__version__,
-    packages = find_packages(),
-    scripts  = ['bin/pikka-bird-server'],
+    
+    include_package_data = True,
+    packages             = find_packages(),
+    scripts              = [
+        'bin/pikka-bird-server'],
     
     author       = 'tiredpixel',
     author_email = 'tp@tiredpixel.com',
+    classifiers  = [
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+    ],
     description  = "Pikka Bird ops monitoring tool Server component.",
+    keywords     = 'ops monitoring sysadmin',
     license      = 'MIT',
-    keywords     = 'monitoring',
     url          = 'https://github.com/tiredpixel/pikka-bird-server-py',
     
     install_requires = [
+        'Flask',
+        'Flask-Migrate',
+        'Flask-Script',
+        'Flask-SQLAlchemy',
+        'gunicorn',
+        'msgpack-python',
+        'psycopg2',
     ]
 )
